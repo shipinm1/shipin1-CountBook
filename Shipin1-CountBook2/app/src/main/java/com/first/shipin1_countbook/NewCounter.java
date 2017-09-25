d0372bfed4d25245176f6a6e4f61cedf5f3ab348
shipinm1 <shipin1@ualberta.ca>
2017-09-24 19:15:19 -0600

    correct intent

--- Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/NewCounter.java
+++ Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/NewCounter.java
@@ -1,6 +1,7 @@
 package com.first.shipin1_countbook;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -8,7 +9,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class NewCounter extends AppCompatActivity{
-    public void onCreate(){
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.add_new_counter);
         //Intent intent = getIntent();
 
