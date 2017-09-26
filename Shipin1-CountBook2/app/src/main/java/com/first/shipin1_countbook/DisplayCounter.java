08704edf7d89c781627039c65429e424707dec8f
shipinm1 <shipin1@ualberta.ca>
2017-09-26 01:02:42 -0600

    clickable

--- Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/DisplayCounter.java
+++ Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/DisplayCounter.java
@@ -2,6 +2,7 @@ package com.first.shipin1_countbook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by gsp on 2017/9/24.
@@ -12,6 +13,11 @@ public class DisplayCounter extends AppCompatActivity{
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.counter_activity);
+        Bundle bundle = getIntent().getExtras();
+        TextView txtview = (TextView) findViewById(R.id.CounterTextView);
+        txtview.setText(bundle.getString("CounterName"));
+        
+
     }
 
 }
