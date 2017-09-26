267622e8b67ed4ea2e131afbce31663c1a66463a
shipinm1 <shipin1@ualberta.ca>
2017-09-25 23:43:36 -0600

    viewlist clickable

--- Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/DisplayCounter.java
+++ Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/DisplayCounter.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class DisplayCounter extends AppCompatActivity{
+    @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.counter_activity);
--- Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/MainActivity.java
+++ Shipin1-CountBook2/app/src/main/java/com/first/shipin1_countbook/MainActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     //public static final String EXTRA_MESSAGE="com.first.shipin1_countbook.MESSAGE";
@@ -23,11 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        counterlist.add("food");
-        counterlist.add("water");
-        counterlist.add("gear");
-        counterlist.add("gun");
+        Date date = new Date();
+        counterlist.add("food"+"\nModified on"+date);
+        counterlist.add("water"+"\nModified on"+date);
+        counterlist.add("gear"+"\nModified on"+date);
+        counterlist.add("gun"+"\nModified on"+date);
+        counterlist.add("sun");
 
         counterList = (ListView) findViewById(R.id.counterList);
         ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, counterlist);
@@ -35,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
         TextView totalCounterNumber = (TextView) findViewById(R.id.totalCounter);
         totalCounterNumber.setText("Total number of Counter: " + counterList.getAdapter().getCount());
 
+        counterList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView,View view, int pos, long arg3){
+                Intent intent = new Intent(MainActivity.this, DisplayCounter.class);
+                intent.putExtra("CounterName", counterList.getItemAtPosition(pos).toString());
+                    startActivity(intent);
+            }
+        });
 
     }
 
